fbb89d8787b3916eabc6f25c4e060c66300bf700
Nikita Bosov <neckbosov@gmail.com>
2019-03-30 21:29:34 +0300

    Add ASTNodeVisitor abstract class

--- task06/model.py
+++ task06/model.py
@@ -19,6 +19,48 @@ class Scope:
         self.items[var_name] = value
 
 
+class ASTNodeVisitor(metaclass=abc.ABCMeta):
+    @abc.abstractmethod
+    def visit_number(self, number):
+        pass
+
+    @abc.abstractmethod
+    def visit_function(self, function):
+        pass
+
+    @abc.abstractmethod
+    def visit_function_definition(self, function_definition):
+        pass
+
+    @abc.abstractmethod
+    def visit_conditional(self, conditional):
+        pass
+
+    @abc.abstractmethod
+    def visit_print(self, print_object):
+        pass
+
+    @abc.abstractmethod
+    def visit_read(self, read_object):
+        pass
+
+    @abc.abstractmethod
+    def visit_function_call(self, function_call):
+        pass
+
+    @abc.abstractmethod
+    def visit_reference(self, reference):
+        pass
+
+    @abc.abstractmethod
+    def visit_binary_operation(self, binary_operation):
+        pass
+
+    @abc.abstractmethod
+    def visit_unary_operation(self, unary_operation):
+        pass
+
+
 class ASTNode(metaclass=abc.ABCMeta):
     @abc.abstractmethod
     def evaluate(self, scope):
