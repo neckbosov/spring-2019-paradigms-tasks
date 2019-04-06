cfa246bd9a0195b2ca9303571f0500cdd0acbf95
Nikita Bosov <neckbosov@gmail.com>
2019-04-06 20:14:28 +0300

    Fix pycodestyle errors

--- task06/printer.py
+++ task06/printer.py
@@ -45,13 +45,13 @@ class ExpressionPrinter(model.ASTNodeVisitor):
         return reference.var_name
 
     def visit_binary_operation(self, binary_operation):
-        return ('(' + binary_operation.left.accept(self) + 
-            ') ' + binary_operation.op + 
-            ' (' + binary_operation.right.accept(self) + ')')
+        return ('(' + binary_operation.left.accept(self) +
+                ') ' + binary_operation.op +
+                ' (' + binary_operation.right.accept(self) + ')')
 
     def visit_unary_operation(self, unary_operation):
-        return (unary_operation.op + 
-            '(' + unary_operation.expr.accept(self) + ')')
+        return (unary_operation.op +
+                '(' + unary_operation.expr.accept(self) + ')')
 
 
 class PrettyPrinter(model.ASTNodeVisitor):
@@ -82,8 +82,8 @@ class PrettyPrinter(model.ASTNodeVisitor):
         return result
 
     def visit_print(self, print_object):
-        return ('print ' + 
-            print_object.expr.accept(self.expression_printer) + ';')
+        return ('print ' +
+                print_object.expr.accept(self.expression_printer) + ';')
 
     def visit_read(self, read_object):
         return 'read ' + read_object.var_name + ';'
