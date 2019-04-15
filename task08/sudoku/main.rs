fa055e8dbdee4067cc6afa858fc0d1289ac8dea3
Nikita Bosov <neckbosov@gmail.com>
2019-04-15 18:49:44 +0300

    Add channel

--- task08/sudoku/main.rs
+++ task08/sudoku/main.rs
@@ -6,10 +6,10 @@
 
 // Намек компилятору, что мы также хотим использовать наш модуль из файла `field.rs`.
 mod field;
-
 // Чтобы не писать `field::Cell:Empty`, можно "заимпортировать" нужные вещи из модуля.
 use field::Cell::*;
 use field::{parse_field, Field, N};
+use std::sync::mpsc::channel;
 
 /// Эта функция выполняет один шаг перебора в поисках решения головоломки.
 /// Она перебирает значение какой-нибудь пустой клетки на поле всеми непротиворечивыми способами.
@@ -168,8 +168,9 @@ fn find_solution(f: &mut Field) -> Option<Field> {
 /// Если хотя бы одно решение `s` существует, возвращает `Some(s)`,
 /// в противном случае возвращает `None`.
 fn find_solution_parallel(mut f: Field) -> Option<Field> {
-    // TODO: вам требуется изменить эту функцию.
-    find_solution(&mut f)
+    let (tx, rx) = channel();
+    tx.send(find_solution(&mut f)).unwrap();
+    rx.recv().unwrap()
 }
 
 /// Юнит-тест, проверяющий, что `find_solution()` находит лексикографически минимальное решение на пустом поле.
