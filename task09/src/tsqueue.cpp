b1a89810381b4f426c3eb35a11d20111f7d67be1
Nikita Bosov <neckbosov@gmail.com>
2019-04-26 10:09:02 +0300

    Rename conditional variable

--- task09/src/tsqueue.cpp
+++ task09/src/tsqueue.cpp
@@ -2,11 +2,11 @@
 void threadsafe_queue_init(ThreadsafeQueue *q) {
     queue_init(&(q->q));
     pthread_mutex_init(&q->m, nullptr);
-    pthread_cond_init(&q->cond, nullptr);
+    pthread_cond_init(&q->cond_queue_not_empty, nullptr);
 }
 
 void threadsafe_queue_destroy(ThreadsafeQueue *q) {
-    pthread_cond_destroy(&q->cond);
+    pthread_cond_destroy(&q->cond_queue_not_empty);
     pthread_mutex_destroy(&q->m);
     queue_destroy(&q->q);
 }
@@ -14,14 +14,14 @@ void threadsafe_queue_destroy(ThreadsafeQueue *q) {
 void threadsafe_queue_push(ThreadsafeQueue *q, void *data) {
     pthread_mutex_lock(&(q->m));
     queue_push(&(q->q), data);
-    pthread_cond_signal(&q->cond);
+    pthread_cond_signal(&q->cond_queue_not_empty);
     pthread_mutex_unlock(&q->m);
 }
 
 void *threadsafe_queue_wait_and_pop(ThreadsafeQueue *q) {
     pthread_mutex_lock(&(q->m));
     while (queue_empty(&(q->q))) {
-        pthread_cond_wait(&q->cond, &q->m);
+        pthread_cond_wait(&q->cond_queue_not_empty, &q->m);
     }
     void *result = queue_pop(&q->q);
     pthread_mutex_unlock(&q->m);
