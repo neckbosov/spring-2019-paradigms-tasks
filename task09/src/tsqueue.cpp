b89e6861850a5c8f846e86d9178da1686bdb76ef
Nikita Bosov <neckbosov@gmail.com>
2019-04-27 00:22:39 +0300

    Remove unnecessary brackets

--- task09/src/tsqueue.cpp
+++ task09/src/tsqueue.cpp
@@ -1,6 +1,6 @@
 #include "tsqueue.h"
 void threadsafe_queue_init(ThreadsafeQueue *q) {
-    queue_init(&(q->q));
+    queue_init(&q->q);
     pthread_mutex_init(&q->m, nullptr);
     pthread_cond_init(&q->cond_queue_not_empty, nullptr);
 }
@@ -12,15 +12,15 @@ void threadsafe_queue_destroy(ThreadsafeQueue *q) {
 }
 
 void threadsafe_queue_push(ThreadsafeQueue *q, void *data) {
-    pthread_mutex_lock(&(q->m));
-    queue_push(&(q->q), data);
+    pthread_mutex_lock(&q->m);
+    queue_push(&q->q, data);
     pthread_cond_signal(&q->cond_queue_not_empty);
     pthread_mutex_unlock(&q->m);
 }
 
 void *threadsafe_queue_wait_and_pop(ThreadsafeQueue *q) {
-    pthread_mutex_lock(&(q->m));
-    while (queue_empty(&(q->q))) {
+    pthread_mutex_lock(&q->m);
+    while (queue_empty(&q->q)) {
         pthread_cond_wait(&q->cond_queue_not_empty, &q->m);
     }
     void *result = queue_pop(&q->q);
