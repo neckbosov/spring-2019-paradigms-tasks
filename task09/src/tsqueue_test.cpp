8e3329b3c3219462adf231e9bae92dd41ea43107
Nikita Bosov <neckbosov@gmail.com>
2019-04-26 19:42:19 +0300

    Format tsqueue_test

--- task09/src/tsqueue_test.cpp
+++ task09/src/tsqueue_test.cpp
@@ -57,7 +57,8 @@ TEST_CASE("ThreadsafeQueue multithreaded ping-pong") {
     auto ponger = [](void *_qs) -> void * {
         ThreadsafeQueue *qs = static_cast<ThreadsafeQueue *>(_qs);
         for (int i = 0; i < PING_PONGS; ++i) {
-            int *val = static_cast<int *>(threadsafe_queue_wait_and_pop(&qs[0]));
+            int *val =
+                static_cast<int *>(threadsafe_queue_wait_and_pop(&qs[0]));
             *val += 1;
             threadsafe_queue_push(&qs[1], val);
         }
@@ -67,6 +68,7 @@ TEST_CASE("ThreadsafeQueue multithreaded ping-pong") {
     pthread_t t1, t2;
     REQUIRE(pthread_create(&t1, nullptr, pinger, qs) == 0);
     REQUIRE(pthread_create(&t2, nullptr, ponger, qs) == 0);
+    
     REQUIRE(pthread_join(t2, nullptr) == 0);
     REQUIRE(pthread_join(t1, nullptr) == 0);
     threadsafe_queue_destroy(&qs[1]);
