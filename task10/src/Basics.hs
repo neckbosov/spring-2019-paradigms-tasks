09c39f595c3bb59daae457ec624bddcad97a5fb5
Nikita Bosov <neckbosov@gmail.com>
2019-05-03 20:28:55 +0300

    Add Basics

--- task10/src/Basics.hs
+++ task10/src/Basics.hs
@@ -9,39 +9,48 @@ import Prelude hiding (head, tail, take, drop, filter, foldl, concat, (++))
 
 -- 1. head' возвращает первый элемент непустого списка
 head' :: [a] -> a
-head' = undefined
+head' (x : _) = x
 
 -- 2. tail' возвращает список без первого элемента, для пустого - пустой
 tail' :: [a] -> [a]
-tail' = undefined
+tail' (_ : xs) = xs
 
 -- 3. take' возвращает первые n >= 0 элементов исходного списка
 take' :: Int -> [a] -> [a]
-take' = undefined
+take' 0 _ = []
+take' n (x : xs)  = x : take' (n - 1) xs
 
 -- 4. drop' возвращает список без первых n >= 0 элементов; если n больше длины
 -- списка, то пустой список.
 drop' :: Int -> [a] -> [a]
-drop' = undefined
+drop' 0 x = x
+drop' _ [] = []
+drop' n (_ : xs) = drop' (n - 1) xs
 
 -- 5. filter' возвращает список из элементов, для которых f возвращает True
 filter' :: (a -> Bool) -> [a] -> [a]
-filter' f xs = undefined
+filter' _ [] = []
+filter' f (x : xs) = if f x
+                   then x : filter' f xs
+                   else filter' f xs
 
 -- 6. foldl'' последовательно применяет функцию f к элементу списка l и значению,
 -- полученному на предыдущем шаге, начальное значение
 -- foldl'' (+) 0 [1, 2, 3] == (((0 + 1) + 2) + 3)
 -- foldl'' (*) 4 [] == 4
 foldl'' :: (a -> b -> a) -> a -> [b] -> a
-foldl'' f z l = undefined
+foldl'' _ z [] = z
+foldl'' f z (x : xs) = foldl'' f (f z x) xs
 
 -- 7. concat' принимает на вход два списка и возвращает их конкатенацию
 -- concat' [1,2] [3] == [1,2,3]
 concat' :: [a] -> [a] -> [a]
-concat' = undefined
+concat' [] b = b
+concat' (x : xs) b = x : concat' xs b
 
 -- 8. quickSort' возвращает его отсортированный список
 -- quickSort' должен быть реализован через алгоритм QuickSort
 -- (выбор pivot может быть любым)
 quickSort' :: Ord a => [a] -> [a]
-quickSort' = undefined
+quickSort' [] = []
+quickSort' (x : xs) = concat' (quickSort' (filter' (<= x) xs)) (quickSort' (filter' (> x) xs))
