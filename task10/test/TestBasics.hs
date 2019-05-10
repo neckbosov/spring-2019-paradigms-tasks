f3c264501ec0ac5762a04fd4b27a8dea760ccc68
Nikita Bosov <neckbosov@gmail.com>
2019-05-10 17:14:07 +0300

    Reorder tests

--- task10/test/TestBasics.hs
+++ task10/test/TestBasics.hs
@@ -11,42 +11,42 @@ testsBasics = testGroup "Unit tests for Basics tasks"
     [testCase "head' works on non-empty list" $
         head' [1,2,3] @?= 1
 
+    , testCase "head' works on infinite list" $
+            head' [5..] @?= 5
+
     , testCase "tail' works on non-empty list too" $
         tail' [1,2,3] @?= [2,3]
+
+    , testCase "tail' works on infinite list" $
+        (head' . tail') [5..] @?= 6
 
     , testCase "take' takes 1 element from 3-element list" $
         take' 1 [1,2,3] @?= [1]
 
+    , testCase "take' works on infinite list" $
+        take' 4 [5..] @?= [5..8]
+
     , testCase "drop' drops 1 element from 3-element list" $
         drop' 1 [1,2,3] @?= [2,3]
 
+    , testCase "drop' works on infinite list" $
+        head' (drop' 4 [5..]) @?= 9  
+    
     , testCase "filter' selects only even numbers from 0 to 10" $
         filter' even [0..10] @?= [0,2..10]
-
+        
+    , testCase "filter' works on infinite list" $
+            head' (filter' (\x -> x `mod` 4 == 0) [5..]) @?= 8
+        
     , testCase "foldl'' can be used for finding sum of elements" $
         foldl'' (+) 0 [1,2,3] @?= 6
 
     , testCase "concat' works on finite lists as expected" $
         concat' [1,2,3] [4,5,6] @?= [1..6]
 
-    , testCase "quickSort actualy sorts the list" $
-        quickSort' [5,2,3,4,1] @?= [1..5]
-
-    , testCase "head' works on infinite list" $
-        head' [5..] @?= 5
-     
-    , testCase "tail' works on infinite list" $
-        (head' . tail') [5..] @?= 6
-
-    , testCase "take' works on infinite list" $
-        take' 4 [5..] @?= [5..8]
-
-    , testCase "drop' works on infinite list" $
-        head' (drop' 4 [5..]) @?= 9  
-    
-    , testCase "filter' works on infinite list" $
-        head' (filter' (\x -> x `mod` 4 == 0) [5..]) @?= 8
-    
     , testCase "concat' works on infinite lists" $
         head' (concat' [3..] [5..]) @?= 3
+        
+    , testCase "quickSort actualy sorts the list" $
+        quickSort' [5,2,3,4,1] @?= [1..5]
     ]
