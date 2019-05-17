3de23597a55857b1ff3df45b79b98175132d715f
Nikita Bosov <neckbosov@gmail.com>
2019-05-17 23:22:18 +0300

    Fix tests

--- task11/test/TestMap.hs
+++ task11/test/TestMap.hs
@@ -29,81 +29,92 @@ mapTests name (_ :: Proxy m) =
     -- Чтобы можно было связать типовую переменную m здесь и в let ниже, нужно расширение ScopedTypeVariables.
     testGroup name [
         testGroup "Smoke tests" [
-            testCase "Test empty" $
+            testCase "empty" $
                 let tr = empty :: m Int String in Map.null tr @?= True
             ,
-            testCase "Test singleton" $
-                let tr = singleton 6 "a" :: m Int String in 
-                (size tr == 1) && member 6 tr && (fromJust (Map.lookup 6 tr) == "a") @?= True
+            testCase "singleton" $
+                let tr = singleton 6 "a" :: m Int String in do
+                size tr @?= 1
+                member 6 tr @?= True
+                fromJust (Map.lookup 6 tr) @?= "a"
             ,
-            testCase "Test fromList" $
-                let tr = fromList [(6, "a"), (6, "b")] :: m Int String in 
-                (size tr == 1) && member 6 tr && (fromJust (Map.lookup 6 tr) == "b") @?= True
+            testCase "fromList" $
+                let tr = fromList [(6, "a"), (6, "b")] :: m Int String in do
+                size tr @?= 1
+                member 6 tr @?= True
+                fromJust (Map.lookup 6 tr) @?= "b"
             ,
             testCase "toAscList . fromList sorts list" $
                 let tr = fromList [(2, "a"), (1, "b"), (3, "c"), (1, "x")] :: m Int String in
                 toAscList tr @?= [(1, "x"), (2, "a"), (3, "c")]
             ,
-            testCase "Test insert" $
-                let tr = insert 6 "a" empty :: m Int String in 
-                (size tr == 1) && member 6 tr && (fromJust (Map.lookup 6 tr) == "a") @?= True
+            testCase "insert" $
+                let tr = insert 6 "a" empty :: m Int String in do
+                size tr @?= 1
+                member 6 tr @?= True
+                fromJust (Map.lookup 6 tr) @?= "a"
             ,
-            testCase "Test insertWith" $
-                let tr = insertWith (++) 6 "b" (singleton 6 "a") :: m Int String in 
-                (size tr == 1) && member 6 tr && (fromJust (Map.lookup 6 tr) == "ba") @?= True
+            testCase "insertWith" $
+                let tr = insertWith (++) 6 "b" (singleton 6 "a") :: m Int String in do
+                size tr @?= 1
+                member 6 tr @?= True
+                fromJust (Map.lookup 6 tr) @?= "ba"
             ,
-            testCase "Test insertWithKey" $
-                let tr = insertWithKey (\k new_v old_v -> show k ++ new_v ++ old_v) 6 "b" (singleton 6 "a") :: m Int String in 
-                (size tr == 1) && member 6 tr && (fromJust (Map.lookup 6 tr) == "6ba") @?= True
+            testCase "insertWithKey" $
+                let tr = insertWithKey (\k newV oldV -> show k ++ newV ++ oldV) 6 "b" (singleton 6 "a") :: m Int String in do
+                size tr @?= 1
+                member 6 tr @?= True
+                fromJust (Map.lookup 6 tr) @?= "6ba"
             ,
-            testCase "Test delete" $
-                let tr = delete 6 (insert 6 "a" empty) :: m Int String in 
-                Map.null tr && isNothing (Map.lookup 6 tr) @?= True
+            testCase "delete" $
+                let tr = delete 6 (insert 6 "a" empty) :: m Int String in do
+                Map.null tr @?= True
+                isNothing (Map.lookup 6 tr) @?= True
             ,
-            testCase "Test adjust" $
+            testCase "adjust" $
                 let tr = adjust ("b" ++) 6 (singleton 6 "a") :: m Int String in 
                 fromJust (Map.lookup 6 tr) @?= "ba"
             ,
-            testCase "Test adjustWithKey" $
+            testCase "adjustWithKey" $
                 let tr = adjustWithKey (\k v -> show k ++ v) 6 (singleton 6 "a") :: m Int String in 
                 fromJust (Map.lookup 6 tr) @?= "6a"
             ,
-            testCase "Test update" $
+            testCase "update" $
                 let tr = update (const Nothing) 6 (singleton 6 "a") :: m Int String in 
                 Map.null tr @?= True
             ,
-            testCase "Test updateWithKey" $
+            testCase "updateWithKey" $
                 let tr = updateWithKey (\k v -> Just $ show k ++ v) 6 (singleton 6 "a") :: m Int String in 
                 fromJust (Map.lookup 6 tr) @?= "6a"
             ,
-            testCase "Test alter" $
+            testCase "alter" $
                 let tr = alter (fmap $ (++) "b") 6 (singleton 6 "a") :: m Int String in 
                 fromJust (Map.lookup 6 tr) @?= "ba"
             ,
-            testCase "Test lookup" $
+            testCase "lookup" $
                 let tr = singleton 6 "a" :: m Int String in 
-                    fromJust (Map.lookup 6 tr) @?= "a"
-                ,
-            testCase "Test member" $
+                fromJust (Map.lookup 6 tr) @?= "a"
+            ,
+            testCase "member" $
                 let tr = singleton 6 "a" :: m Int String in 
-                    member 6 tr @?= True
-                ,
-            testCase "Test notMember" $
+                member 6 tr @?= True
+            ,
+            testCase "notMember" $
                 let tr = singleton 6 "a" :: m Int String in 
-                    notMember 4 tr @?= True
-                ,
-            testCase "Test null" $
+                notMember 4 tr @?= True
+            ,
+            testCase "null" $
                 let tr = singleton 6 "a" :: m Int String in 
-                    Map.null tr @?= False
-                ,
-            testCase "Test size" $
+                Map.null tr @?= False
+            ,
+            testCase "size" $
                 let tr = fromList [(2, "a"), (1, "b"), (3, "c"), (1, "x")] :: m Int String in
-                    size tr @?= 3
+                size tr @?= 3
         ]
     ]
 
 testNaiveTree :: TestTree
-testNaiveTree = testGroup "Test NaiveTree" [
+testNaiveTree = testGroup "NaiveTree" [
         testGroup "merge" [
             testCase "merge empty" $
                 merge Nil Nil @?= (Nil :: NaiveTree () ())
