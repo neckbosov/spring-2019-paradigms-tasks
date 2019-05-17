22ce0a471bb08cb72d77dd510d496b215daafeaf
Nikita Bosov <neckbosov@gmail.com>
2019-05-17 20:44:12 +0300

    Add smoke tests

--- task11/test/TestMap.hs
+++ task11/test/TestMap.hs
@@ -2,6 +2,7 @@
 import Test.Tasty
 import Test.Tasty.HUnit
 import Data.Proxy
+import Data.Maybe
 import Map
 import qualified Data.Map.Strict as SMap
 import MapInstance
@@ -28,9 +29,72 @@ mapTests name (_ :: Proxy m) =
     -- Чтобы можно было связать типовую переменную m здесь и в let ниже, нужно расширение ScopedTypeVariables.
     testGroup name [
         testGroup "Smoke tests" [
+            testCase "Test empty" $
+                let tr = empty :: m Int String in Map.null tr @?= True
+            ,
+            testCase "Test singleton" $
+                let tr = singleton 6 "a" :: m Int String in 
+                (size tr == 1) && member 6 tr && (fromJust (Map.lookup 6 tr) == "a") @?= True
+            ,
+            testCase "Test fromList" $
+                let tr = fromList [(6, "a"), (6, "b")] :: m Int String in 
+                (size tr == 1) && member 6 tr && (fromJust (Map.lookup 6 tr) == "b") @?= True
+            ,
             testCase "toAscList . fromList sorts list" $
                 let tr = fromList [(2, "a"), (1, "b"), (3, "c"), (1, "x")] :: m Int String in
                 toAscList tr @?= [(1, "x"), (2, "a"), (3, "c")]
+            ,
+            testCase "Test insert" $
+                let tr = insert 6 "a" empty :: m Int String in 
+                (size tr == 1) && member 6 tr && (fromJust (Map.lookup 6 tr) == "a") @?= True
+            ,
+            testCase "Test insertWith" $
+                let tr = insertWith (++) 6 "b" (singleton 6 "a") :: m Int String in 
+                (size tr == 1) && member 6 tr && (fromJust (Map.lookup 6 tr) == "ba") @?= True
+            ,
+            testCase "Test insertWithKey" $
+                let tr = insertWithKey (\k new_v old_v -> show k ++ new_v ++ old_v) 6 "b" (singleton 6 "a") :: m Int String in 
+                (size tr == 1) && member 6 tr && (fromJust (Map.lookup 6 tr) == "6ba") @?= True
+            ,
+            testCase "Test delete" $
+                let tr = delete 6 (insert 6 "a" empty) :: m Int String in 
+                Map.null tr && isNothing (Map.lookup 6 tr) @?= True
+            ,
+            testCase "Test adjust" $
+                let tr = adjust ("b" ++) 6 (singleton 6 "a") :: m Int String in 
+                fromJust (Map.lookup 6 tr) @?= "ba"
+            ,
+            testCase "Test adjustWithKey" $
+                let tr = adjustWithKey (\k v -> show k ++ v) 6 (singleton 6 "a") :: m Int String in 
+                fromJust (Map.lookup 6 tr) @?= "6a"
+            ,
+            testCase "Test alter" $
+                let tr = alter (fmap $ (++) "b") 6 (singleton 6 "a") :: m Int String in 
+                fromJust (Map.lookup 6 tr) @?= "ba"
+            ,
+            testCase "Test lookup" $
+            let tr = singleton 6 "a" :: m Int String in 
+                fromJust (Map.lookup 6 tr) @?= "a"
+            ,
+            testCase "Test lookup" $
+            let tr = singleton 6 "a" :: m Int String in 
+                fromJust (Map.lookup 6 tr) @?= "a"
+            ,
+            testCase "Test member" $
+            let tr = singleton 6 "a" :: m Int String in 
+                member 6 tr @?= True
+            ,
+            testCase "Test notMember" $
+            let tr = singleton 6 "a" :: m Int String in 
+                notMember 4 tr @?= True
+            ,
+            testCase "Test null" $
+            let tr = singleton 6 "a" :: m Int String in 
+                Map.null tr @?= False
+            ,
+            testCase "Test size" $
+            let tr = fromList [(2, "a"), (1, "b"), (3, "c"), (1, "x")] :: m Int String in
+                size tr @?= 3
         ]
     ]
 
