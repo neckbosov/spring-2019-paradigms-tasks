96693145111baa244558d43627ef1132f6004a8e
Nikita Bosov <neckbosov@gmail.com>
2019-05-21 19:49:31 +0300

    Add 02.sql

--- task12/02.sql
+++ task12/02.sql
@@ -1,4 +1,5 @@
 -- Выведите название страны с максимальным уровнем грамотности по последним
 -- данным, которые доступны для страны. В выводе: название страны и уровень
 -- грамотности, именно в таком порядке и без лишних полей. (0,75 баллов)
-SELECT * FROM Country;
+SELECT Country.Name, MAX(LiteracyRate.Rate) FROM LiteracyRate
+JOIN Country ON Country.Code = LiteracyRate.CountryCode;
