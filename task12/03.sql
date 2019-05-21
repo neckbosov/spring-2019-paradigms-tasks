67072ea70a77d6924c3e49d9390dacf8a9afc089
Nikita Bosov <neckbosov@gmail.com>
2019-05-21 19:57:50 +0300

    Add 03.sql

--- task12/03.sql
+++ task12/03.sql
@@ -1,3 +1,6 @@
 -- Выведите столицу Малайзии (Malaysia) (в выводе: только название города).
 -- (0,5 баллов)
-SELECT * FROM Country;
+SELECT City.Name FROM City
+JOIN Country ON Country.Code = City.CountryCode
+JOIN Capital ON Capital.CountryCode = Country.Code
+WHERE Country.Name = 'Malaysia' AND City.Id = Capital.CityId;
